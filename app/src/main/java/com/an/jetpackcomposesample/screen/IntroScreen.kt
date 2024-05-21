f4113b849af37b96e34f164bb35d407e32f39049
Anitaa Murthy <murthyanitaa@gmail.com>
2024-05-21 12:23:20 +0530

    Moved screen files into it's own package

--- app/src/main/java/com/an/jetpackcomposesample/screen/IntroScreen.kt
+++ app/src/main/java/com/an/jetpackcomposesample/screen/IntroScreen.kt
@@ -1,4 +1,4 @@
-package com.an.jetpackcomposesample
+package com.an.jetpackcomposesample.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.an.jetpackcomposesample.R
 import com.an.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 
 @Composable
--- app/src/main/java/com/an/jetpackcomposesample/screen/RowScreen.kt
+++ app/src/main/java/com/an/jetpackcomposesample/screen/RowScreen.kt
@@ -1,4 +1,4 @@
-package com.an.jetpackcomposesample
+package com.an.jetpackcomposesample.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.an.jetpackcomposesample.R
 import com.an.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 import com.an.jetpackcomposesample.ui.theme.Purple40
 import com.an.jetpackcomposesample.ui.theme.Purple60
