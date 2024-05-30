2d2053cd285e16e81b0b87167ff121212d47e1ca
Anitaa Murthy <murthyanitaa@gmail.com>
2024-05-30 17:17:32 +0530

    Added custom dialog

--- app/src/main/java/com/an/jetpackcomposesample/screen/DialogScreen.kt
+++ app/src/main/java/com/an/jetpackcomposesample/screen/DialogScreen.kt
@@ -1,15 +1,20 @@
 package com.an.jetpackcomposesample.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -17,14 +22,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.an.jetpackcomposesample.ui.theme.Purple40
+import com.an.jetpackcomposesample.ui.theme.Purple60
 import com.an.jetpackcomposesample.ui.theme.Purple80
 
 @Composable
 fun DialogScreen(modifier: Modifier = Modifier) {
     val showAlertDialog = remember { mutableStateOf(false) }
+    val showCustomDialog = remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
@@ -36,10 +49,16 @@ fun DialogScreen(modifier: Modifier = Modifier) {
         Button(onClick = { showAlertDialog.value = true }) {
             Text("Alert Dialog")
         }
+        Button(onClick = { showCustomDialog.value = true }) {
+            Text("Custom Dialog")
+        }
     }
 
     if(showAlertDialog.value) {
         AlertDialogExample(showAlertDialog)
+    }
+    if(showCustomDialog.value) {
+        CustomDialogExample(showCustomDialog)
     }
 }
 
@@ -71,4 +90,53 @@ fun AlertDialogExample(showAlertDialog: MutableState<Boolean>) {
             }
         }
     )
+}
+
+@Composable
+fun CustomDialogExample(showCustomDialog: MutableState<Boolean>) {
+    Dialog(onDismissRequest = { showCustomDialog.value = false }) {
+        Card(
+            elevation = CardDefaults.cardElevation(8.dp),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp, top = 20.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(Purple60)
+                    .padding(16.dp)
+            ) {
+                Icon(Icons.Filled.Info, contentDescription = "Info Icon", tint = Purple80)
+                Column(
+                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        text = "Dialog with info icon",
+                        style = TextStyle(fontWeight = FontWeight.Bold, color = Purple40, fontSize = 18.sp)
+                    )
+                    Text(
+                        modifier = Modifier.padding(top = 10.dp),
+                        textAlign = TextAlign.Center,
+                        text = "This Dialog is an example of a custom dialog with the Dialog Composable"
+                    )
+                }
+                TextButton(
+                    onClick = { showCustomDialog.value = false }
+                ) {
+                    Text("Confirm", color = Purple40)
+                }
+                TextButton(
+                    onClick = { showCustomDialog.value = false }
+                ) {
+                    Text("Dismiss", color = Purple40)
+                }
+            }
+        }
+    }
 }
