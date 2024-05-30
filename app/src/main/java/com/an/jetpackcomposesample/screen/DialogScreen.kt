6392baf111b990d6df3e0bb08a2890f771eaf117
Anitaa Murthy <murthyanitaa@gmail.com>
2024-05-30 18:50:58 +0530

    Added BottomSheet screen

--- app/src/main/java/com/an/jetpackcomposesample/screen/DialogScreen.kt
+++ app/src/main/java/com/an/jetpackcomposesample/screen/DialogScreen.kt
@@ -1,17 +1,22 @@
 package com.an.jetpackcomposesample.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,15 +24,18 @@ import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -43,6 +51,7 @@ fun DialogScreen(modifier: Modifier = Modifier) {
     val showAlertDialog = remember { mutableStateOf(false) }
     val showCustomDialog = remember { mutableStateOf(false) }
     val datePickerDialog = remember { mutableStateOf(false) }
+    val bottomSheetDialog = remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
@@ -60,6 +69,9 @@ fun DialogScreen(modifier: Modifier = Modifier) {
         Button(onClick = { datePickerDialog.value = true }) {
             Text("DatePicker Dialog")
         }
+        Button(onClick = { bottomSheetDialog.value = true }) {
+            Text("Bottom Sheet Dialog")
+        }
     }
 
     if(showAlertDialog.value) {
@@ -70,6 +82,9 @@ fun DialogScreen(modifier: Modifier = Modifier) {
     }
     if(datePickerDialog.value) {
         DatePickerDialogExample(datePickerDialog)
+    }
+    if(bottomSheetDialog.value) {
+        BottomSheetExample(bottomSheetDialog)
     }
 }
 
@@ -180,4 +195,72 @@ fun DatePickerDialogExample(showDatePickerDialog: MutableState<Boolean>) {
             showModeToggle = true
         )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetExample(showBottomSheet: MutableState<Boolean>) {
+    val context = LocalContext.current
+    val modalBottomSheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = { showBottomSheet.value = false },
+        sheetState = modalBottomSheetState,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+    ) {
+        val countries = listOf(
+            Pair("United States", "US".toFlagEmoji()),
+            Pair("Canada", "CA".toFlagEmoji()),
+            Pair("India", "IN".toFlagEmoji()),
+            Pair("Germany", "DE".toFlagEmoji()),
+            Pair("France", "FR".toFlagEmoji()),
+            Pair("Japan", "JP".toFlagEmoji()),
+            Pair("China", "CN".toFlagEmoji()),
+            Pair("Brazil", "BR".toFlagEmoji()),
+            Pair("Australia", "AU".toFlagEmoji()),
+            Pair("Russia", "RU".toFlagEmoji()),
+            Pair("United Kingdom", "UK".toFlagEmoji()),
+            Pair("Belgium", "BE".toFlagEmoji()),
+            Pair("Pakistan", "PK".toFlagEmoji()),
+        )
+
+        LazyColumn {
+            items(countries.size) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp, horizontal = 20.dp)
+                        .clickable {
+                            Toast.makeText(context, countries[it].first+ " Clicked", Toast.LENGTH_SHORT).show()
+                            showBottomSheet.value = false
+                        }
+                ) {
+                    Text(
+                        text = countries[it].second,
+                        modifier = Modifier.padding(end = 20.dp, top = 5.dp, bottom = 5.dp)
+                    )
+                    Text(text = countries[it].first)
+                }
+            }
+        }
+    }
+}
+
+private fun String.toFlagEmoji(): String {
+    // 1. It first checks if the string consists of only 2 characters: ISO 3166-1 alpha-2 two-letter country codes (https://en.wikipedia.org/wiki/Regional_Indicator_Symbol).
+    if (this.length != 2) {
+        return this
+    }
+
+    val countryCodeCaps =
+        this.uppercase() // upper case is important because we are calculating offset
+    val firstLetter = Character.codePointAt(countryCodeCaps, 0) - 0x41 + 0x1F1E6
+    val secondLetter = Character.codePointAt(countryCodeCaps, 1) - 0x41 + 0x1F1E6
+
+    // 2. It then checks if both characters are alphabet
+    if (!countryCodeCaps[0].isLetter() || !countryCodeCaps[1].isLetter()) {
+        return this
+    }
+
+    return String(Character.toChars(firstLetter)) + String(Character.toChars(secondLetter))
 }
