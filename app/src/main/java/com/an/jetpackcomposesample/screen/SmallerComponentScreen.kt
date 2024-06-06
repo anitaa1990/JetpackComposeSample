6cd0e73a4836c27a0e24ddab7198823f1d90405e
Anitaa Murthy <murthyanitaa@gmail.com>
2024-06-06 18:50:01 +0530

    Added chips

--- app/src/main/java/com/an/jetpackcomposesample/screen/SmallerComponentScreen.kt
+++ app/src/main/java/com/an/jetpackcomposesample/screen/SmallerComponentScreen.kt
@@ -1,6 +1,9 @@
 package com.an.jetpackcomposesample.screen
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,11 +12,23 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
@@ -25,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.an.jetpackcomposesample.ui.theme.Purple40
 import com.an.jetpackcomposesample.ui.theme.Purple60
@@ -47,6 +63,10 @@ fun SmallerComponentScreen(modifier: Modifier = Modifier) {
 
         TextHeading("Switch")
         SwitchExample()
+
+        TextHeading("Chips")
+        ChipExample()
+
     }
 }
 
@@ -106,7 +126,9 @@ fun RangeSliderExample() {
 fun SwitchExample() {
     var checked by remember { mutableStateOf(true) }
     Column(
-        modifier = Modifier.padding(12.dp).fillMaxWidth(),
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Switch(
@@ -141,4 +163,150 @@ fun SwitchExample() {
             )
         )
     }
+}
+
+@Composable
+fun ChipExample() {
+    Row (
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        AssistChipExample()
+        FilterChipExample()
+        InputChipExample()
+    }
+    Row (
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        SuggestionChipExample()
+    }
+}
+
+@Composable
+fun AssistChipExample() {
+    val context = LocalContext.current
+
+    AssistChip(
+        onClick = { Toast.makeText(context, "Assist clicked!", Toast.LENGTH_SHORT).show()  },
+        label = {
+            Text(
+                text = "Assist chip",
+                style = MaterialTheme.typography.labelMedium,
+                color = Purple40
+            )
+        },
+        border = AssistChipDefaults.assistChipBorder(
+            borderColor = Purple40,
+        ),
+        leadingIcon = {
+            Icon(
+                Icons.Filled.Settings,
+                contentDescription = "Settings",
+                Modifier.size(AssistChipDefaults.IconSize),
+                tint = Purple40
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilterChipExample() {
+    // Filter Chip
+    var selectedFilter by remember { mutableStateOf(false) }
+    val filterLeadingIcon = if (selectedFilter) Icons.Filled.Clear else Icons.Filled.Check
+    FilterChip(
+        onClick = { selectedFilter = !selectedFilter },
+        border = FilterChipDefaults.filterChipBorder(
+            borderColor = Purple40,
+            selectedBorderColor = Purple80
+        ),
+        colors = FilterChipDefaults.filterChipColors(
+            selectedContainerColor = Purple80
+        ),
+        label = {
+            Text(
+                text = "Filter chip",
+                style = MaterialTheme.typography.labelMedium,
+                color = Purple40
+            )
+        },
+        selected = selectedFilter,
+        leadingIcon = {
+            Icon(
+                imageVector = filterLeadingIcon,
+                contentDescription = "Leading icon",
+                modifier = Modifier.size(FilterChipDefaults.IconSize),
+                tint = Purple40
+            )
+        },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputChipExample() {
+    // Input Chip – You can use the InputChip composable to create chips that result from user interaction. For example, in an email client, when the user is writing an email, an input chip might represent a person whose address the user has entered into the "to:" field.
+    var enabled by remember { mutableStateOf(false) }
+    InputChip(
+        onClick = {
+            enabled = !enabled
+        },
+        label = {
+            Text(
+                text = "Input chip",
+                style = MaterialTheme.typography.labelMedium,
+                color = Purple40
+            )
+        },
+        border = FilterChipDefaults.filterChipBorder(
+            borderColor = Purple40,
+            selectedBorderColor = Purple80
+        ),
+        colors = FilterChipDefaults.filterChipColors(
+            selectedContainerColor = Purple80
+        ),
+        selected = enabled,
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "Input Chip person icon",
+                Modifier.size(InputChipDefaults.AvatarSize),
+                tint = Purple40
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Default.Close,
+                contentDescription = "Input Chip close icon",
+                Modifier.size(InputChipDefaults.AvatarSize),
+                tint = Purple40
+            )
+        },
+    )
+}
+
+@Composable
+fun SuggestionChipExample() {
+    // Suggestion chips present dynamically generated hints.
+
+    val context = LocalContext.current
+    SuggestionChip(
+        onClick = { Toast.makeText(context, "Suggestion clicked!", Toast.LENGTH_SHORT).show() },
+        border = AssistChipDefaults.assistChipBorder(
+            borderColor = Purple40,
+        ),
+        label = {
+            Text(
+                text = "Suggestion chip",
+                style = MaterialTheme.typography.labelMedium,
+                color = Purple40
+            )
+        }
+    )
 }
