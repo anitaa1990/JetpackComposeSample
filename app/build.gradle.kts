166231475e0d322e02186634f4b6eef0ac5e32ee
Anitaa Murthy <murthyanitaa@gmail.com>
2024-11-29 14:03:52 +0530

    Modified version number for new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.an.jetpackcomposesample"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.1"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
